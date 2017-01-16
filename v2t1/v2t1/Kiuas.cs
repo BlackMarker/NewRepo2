c0c7c1e06179373861397a3768eae60e783f204e
K8811 <K8811@ICT-421-WS-23>
2017-01-16 13:46:29 +0200

    kiuas

---
+++ v2t1/v2t1/Kiuas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2t1{
+	class Kiuas{
+		public bool Paalla { get; set; }
+		public float Lampo { get; set; }
+		float kosteus;
+		
+		public float Kosteus{
+			get{
+				return kosteus;
+			}
+			set{
+				kosteus = value;
+				if (kosteus < 0 || kosteus > 100) {
+					kosteus = 0;
+				}
+			}
+		}
+	}
+}
---
+++ v2t1/v2t1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Tehtävä 1
+Tehtävänäsi on ohjelmoida kiukaan toiminta. Kiuas pitää pystyä laittamaan päälle ja pois, sekä sen lämpötilaa että sen antamaa kosteutta pitää pystyä säätämään 
+(arvoja ei ole rajattu).
+
+Ohjelmointi
+
+Toteuta Kiuas-luokan ohjelmointi sekä pääohjelma, jolla luot olion Kiuas-luokasta. Säädä kiuas-oliota erilaisilla arvoilla, 
+jätä Console.WriteLine()-tulostuslauseet ohjelmaasi, jotta kiuas-olion käyttäminen jää näkyville.
+*/
+
+using System;
+
+
+namespace v2t1{
+
+
+	class Program{
+		static void Main(string[] args){
+			TestaaKiuas();
+		}
+
+		//t1 luokan kiuas testaus
+		static void TestaaKiuas(){
+			//luodaan kiuas olio
+			Kiuas kiuas = new Kiuas();
+			//Kiuas tulille ja lämp/kost
+			kiuas.Paalla = true;
+			kiuas.Lampo = 90;
+			kiuas.Kosteus = 50;
+			Console.WriteLine("Kiuas on paalla {0}", kiuas.Paalla);
+			Console.WriteLine("Kiukaan lampotila {0}*C\nKiukaan kosteus {1}%", kiuas.Lampo, kiuas.Kosteus);
+			kiuas.Kosteus = 5000; 
+			Console.WriteLine(kiuas.Kosteus);
+		}
+	}
+}
