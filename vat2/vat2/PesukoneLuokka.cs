e43beff3aee9afd76a29d95a1307365bedc7395c
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-16 19:03:29 +0200

    v2t2 kesken

---
+++ vat2/vat2/PesukoneLuokka.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vat2 {
+	class PesukoneLuokka {
+		public bool TarvitseeHuolto = false;
+		public bool Paalla = false;
+
+	}
+}
---
+++ vat2/vat2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Tehtävä 2
+Tehtävänäsi on ohjelmoida pesukoneen toiminta. Samoin kuin edellinen tehtävä: mitä ominaisuuksia ja toimintoja tekisit Pesukone-luokkaan?
+
+UML
+
+Suunnittele Pesukone-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
+
+Ohjelmointi
+
+Toteuta Pesukone-luokan ohjelmointi sekä pääohjelma, jolla luot olion Pesukone-luokasta. Säädä pesukone-oliota erilaisilla arvoilla, 
+jätä Console.WriteLine()-tulostuslauseet ohjelmaasi, jotta pesukone-olion käyttäminen jää näkyville. 
+Toteuta Pesukone-luokkaan muutamia erilaisia konstruktoreita ja alusta niitä käyttämällä oliota pääohjelmasta käsin.
+*/
+using System;
+
+
+namespace vat2 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Pesukone");
+		}
+	}
+}
