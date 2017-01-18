8e169309cd03ab40a8f55c12cd03c1d370a23a3c
K8811 <K8811@ICT-421-WS-23>
2017-01-18 15:01:49 +0200

    V3T2 valm

---
+++ V3T2/V3T2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3T2 {
+	class Vahvistin {
+		int volyymi;
+
+		public int Volyymi {
+			get {
+				return volyymi;
+			}
+			set {
+				volyymi = value;
+				if ( volyymi > 100) {
+					volyymi = 100;
+				}
+				if (volyymi < 0 ) {
+					volyymi = 0;
+				}
+			}
+		}
+
+	}
+}
---
+++ V3T2/V3T2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Tehtävä 2
+Tehtävänäsi on ohjelmoida yksinkertaisen vahvistimen toiminta, jolla voidaan kontrolloida äänenvoimakkuutta välillä 0-100. Toteuta Vahvistin-luokka 
+ja tee pääohjelma, jolla luot olion Vahvistin-luokasta. Säädä ja kokeile vahvistinta eri äänenvoimakkuuksilla. 
+Käytä Vahvistin-luokassa get- ja set-aksessoreita. get-aksessori palauttaa äänenvoimakkuuden ja set-aksessori rajaa äänenvoimakkuuden arvoa.
+*/
+
+using System;
+
+
+namespace V3T2 {
+	class Program {
+		static void Main(string[] args) {
+			while (true) {
+				Vahvistin volyymi = new Vahvistin();
+
+				Console.WriteLine("Saada aanenvoimakkuutta: ");
+				int haluttu = int.Parse(Console.ReadLine());
+				volyymi.Volyymi = haluttu;
+				Console.WriteLine("Aanenvoimakkuus on: {0}", volyymi.Volyymi);
+			}
+		}
+	}
+}
