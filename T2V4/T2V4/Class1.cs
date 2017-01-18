058d52d1573793c7b64c0c8b91bb2e9353175b33
K8811 <K8811@ICT-421-WS-23>
2017-01-18 11:10:41 +0200

    kesk

---
+++ T2V4/T2V4/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2V4
+{
+    class Vechile{
+        public string nimi { get; set; }
+        public int nopeus { get; set; }
+
+        public int rengas { get; set; }
+
+        public void PrintData(){
+            Console.Write(nimi + "\n" + nopeus + "\n" + rengas);
+
+        }
+
+
+
+    }
+}
---
+++ T2V4/T2V4/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Luo Vehicle-luokka seuraavien tietojen mukaisesti:
+
+ominaisuudet
+
+Name:string
+Speed:int
+Tyres:int
+toiminnot
+
+PrintData(), tulostaa Vehiclen ominaisuudet näytölle
+ToString():string, palauttaa kaikki Vehiclen ominaisuudet yhtenä merkkijonona
+Toteuta Vehicle-luokan ohjelmointi sekä pääohjelma, jolla luot olion Vehicle-luokasta. Muuta olion arvoja ja tulosta olion arvoja konsolille.
+*/
+
+using System;
+
+namespace T2V4
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Vechile auto = new Vechile();
+            auto.PrintData();
+
+        }
+
+        
+    }
+
+
+}
