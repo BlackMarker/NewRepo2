f814688a42680370afeed2211265cc90da4dba1e
K8811 <K8811@ICT-421-WS-23>
2017-02-01 14:14:59 +0200

    v8t1 valm

---
+++ v8t1/v8t1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v8t2 {
+	class Luokka1 {
+		public float LaskeKa(float heittoja) {
+			Console.WriteLine("Saatiin {0}", heittoja);
+			Random luvut1 = new Random();
+			Thread.Sleep(1000);
+			int summa = 0;
+			for (int i = 0; i < heittoja; i++) {
+				int luku = luvut1.Next(2, 6);
+				summa = summa + luku;
+				Console.WriteLine("Arvottiin luku {0}", luku);
+			}
+			float summa2 = (float)summa;
+			float heittoja2 = (float)heittoja;
+			float ka = summa2 / heittoja2;
+			Console.WriteLine("heittoja {0} summa {1}", heittoja2, summa2);
+			return (ka);
+		}
+	}
+}
---
+++ v8t1/v8t1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Toteuta Noppa-luokka siten, että noppaa voidaan heittää. Nopan tulee palauttaa satunnainen luku jokaisella heittokerralla. 
+ * Toteuta pääohjelmassa nopan heittäminen. Kokeile ensin heittää noppaa kerran ja tulosta nopan luku. Toteuta tämän jälkeen ohjelma siten, 
+ * että kysyt käyttäjältä heittojen määrän. Heitä noppaa ja tulosta heittojen lukujen keskiarvo.
+ * */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v8t2 {
+	class Program {
+		static void Main(string[] args) { 
+			Luokka1 Lk = new Luokka1();
+			float tulos = Lk.LaskeKa(int.Parse(Console.ReadLine()));
+			Console.WriteLine("Keskiarvo heitoille oli: {0}", tulos);
+		}
+	}
+}
